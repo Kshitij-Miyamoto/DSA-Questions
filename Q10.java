6a3e94a795c1f2702b9422ed8792f770891db02a
Kshitij Sharma <110044782+Kshitij210@users.noreply.github.com>
2023-08-08 23:01:51 +0530

    New commits

--- Q10.java
+++ Q10.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class Q10 {
-    
-}
+//Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)
+public static void main(String[] args) {
+    System.out.println("enter a number : ");
+    Scanner in = new Scanner(System.in);
+   int input;
+   int sum = 0;
+           while(true){
+               input = in.nextInt();
+               if(input == 0){
+                   break;
+               }
+               sum = sum + input;
+           }
+    System.out.println("Sum of the entered number is : "+ sum);
+        }
+
+    }
+
