544c1f73f9875f15adf61ed80efab8700ca8b76f
Kshitij Sharma <110044782+Kshitij210@users.noreply.github.com>
2023-08-13 01:28:26 +0530

    new commits

--- Q39.java
+++ Q39.java
@@ -8,19 +8,25 @@ public class Q39 {
         int num1 = in.nextInt();
         int num2 = in.nextInt();
         int base = in.nextInt();
+        int rv = 0;
 
-        while(num1 != 0){
-            int dig1 = num1%base;
-            num1 = num1/base;
-            System.out.println(dig1);
+       int c = 0;
+       int p = 1;
+        while(num1 > 0 || num2 > 0 || c > 0 ){
+            int dig1 = num1%10;
+            num1 = num1/10;
+            int dig2 = num2%10;
+            num2 = num2/10;
+            int dig = dig1 + dig2 + c;
+            c = dig/base;
+            dig = dig%base;
+            rv = rv + dig*p;
+            p = p*10;
 
-        while(num2 != 0){
-            int dig2 = num2%base;
-            num2 = num2/base;
-            System.out.println(dig2);
         }
+        System.out.println(rv);
     }
 
     }
-}
+
 
